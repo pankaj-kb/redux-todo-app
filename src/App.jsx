d16eabae7d34776891fb22ffb81605af6a85db2c
pankaj-kb <pankajb2601@gmail.com>
2023-03-19 13:28:23 +0530

    starting with redux todo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className="text-5xl underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
