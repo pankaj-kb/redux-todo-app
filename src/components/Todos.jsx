8f38008f9546236c7a2d801c2bdac94caf2eaf48
pankaj-kb <pankajb2601@gmail.com>
2023-03-21 09:14:25 +0530

    added reducer for editing todo

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,25 +1,66 @@
-import React from 'react'
-import {useSelector} from 'react-redux'
-import {useDispatch} from 'react-redux'
-import {removeTodo} from '../features/todoSlice'
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { addTodo, removeTodo, editTodo } from "../features/todoSlice";
 
 function Todos() {
-    const todos = useSelector((state) => state.todos.todos)
-    const dispatch = useDispatch()
+  const todos = useSelector((state) => state.todos.todos);
+  const dispatch = useDispatch();
+  const [newTodo, setNewTodo] = useState("");
+  const [editing, setEditing] = useState(null);
+  const [editText, setEditText] = useState("");
+
+  const handleAddTodo = () => {
+    dispatch(addTodo(newTodo));
+    setNewTodo("");
+  };
+
+  const handleEdit = (id, text) => {
+    setEditing(id);
+    setEditText(text);
+  };
+
+  const handleSave = (id) => {
+    dispatch(editTodo({ id, text: editText }));
+    setEditing(null);
+    setEditText("");
+  };
+
   return (
     <div>
-        {
-        todos.map((todo) => (
-          <>
-            <h1 key={todo.id}>{todo.text}</h1> 
-            <button
-            onClick={() => dispatch(removeTodo(todo.id))}
-            >X</button>
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          {editing === todo.id ? (
+            <>
+              <input
+                type="text"
+                value={editText}
+                onChange={(e) => setEditText(e.target.value)}
+              />
+              <button onClick={() => handleSave(todo.id)}>Save</button>
             </>
-        ))
-        }
+          ) : (
+            <>
+              <p>{todo.text}</p>
+              <button onClick={() => handleEdit(todo.id, todo.text)}>
+                Edit
+              </button>
+              <button onClick={() => dispatch(removeTodo(todo.id))}>X</button>
+            </>
+          )}
         </div>
-  )
+      ))}
+      <div>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="Add new Task"
+        />
+        <button onClick={handleAddTodo}>Add Task</button>
+      </div>
+    </div>
+  );
 }
 
-export default Todos
+export default Todos;
--- src/features/todoSlice.js
+++ src/features/todoSlice.js
@@ -1,34 +1,37 @@
-import {createSlice} from '@reduxjs/toolkit'
-import { nanoid } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
+import { nanoid } from '@reduxjs/toolkit';
 
 const initialState = {
-    todos: [
-        {id: 1122, text: "Learn JS with Hitesh"},
-        {id: 122, text: "Done with redux"},
-    ]
-}
+  todos: [
+    { id: nanoid(), text: 'Learn JS with Hitesh Sir' },
+    { id: nanoid(), text: 'Done with redux' },
+  ],
+};
 
 export const todoSlice = createSlice({
-    name: 'todo',
-    initialState,
-    reducers: {
-        addTodo: (state, action) => {
-            const todo = {
-                id: nanoid(), //Generate a unique ID
-                text: action.payload
-            };
+  name: 'todo',
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+      };
 
-            state.todos.push(todo)
+      state.todos.push(todo);
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+    editTodo: (state, action) => {
+      const todo = state.todos.find((todo) => todo.id === action.payload.id);
+      if (todo) {
+        todo.text = action.payload.text;
+      }
+    },
+  },
+});
 
-            //Todos.slice(1, 3)
-            //Todo.splice(1, 3)
-        },
-        removeTodo: (state, action) => {
-           state.todos =  state.todos.filter((todo) => todo.id !== action.payload)
-        }
-    }
-})
+export const { addTodo, removeTodo, editTodo } = todoSlice.actions;
 
-export const {addTodo, removeTodo} = todoSlice.actions;
-
-export default todoSlice.reducer
+export default todoSlice.reducer;
